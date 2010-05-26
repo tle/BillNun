5a325383116355174df938042d67be7edf2be2a1
psadik <bryanrcampbell@gmail.com>
2010-05-26 04:07:27 -0500

    merge

--- src/com/testapp/client/Sample_gwt.java
+++ src/com/testapp/client/Sample_gwt.java
@@ -30,7 +30,9 @@ public class Sample_gwt implements EntryPoint {
 	private static LoginInfo loginInfo = null;
 	private VerticalPanel loginPanel = new VerticalPanel();
 	private Label loginLabel = new Label("Sign in using your gmail account");
-	private Anchor signinLink = new Anchor("Sign in");	
+	private Anchor signinLink = new Anchor("Sign in");
+	private VerticalPanel friendsList = new VerticalPanel();
+	
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
 	 */
@@ -94,13 +96,13 @@ public class Sample_gwt implements EntryPoint {
 			public void onSuccess(List<UserAccount> result) {
 				for (final UserAccount account: result) {
 					Button addFriend = new Button("Become Friends with: " + account.getUserName() + " (" + account.getEmail() + ")");
+					testPanel.add(addFriend);
 					addFriend.addClickHandler(new ClickHandler() {
 						@Override
 						public void onClick(ClickEvent event) {
 							greetingService.addFriend(account.getEmail(), new AsyncCallback<Void>() {
 								public void onFailure(Throwable caught) {};
 								public void onSuccess(Void v) {}
-								
 							});
 						}
 					});
@@ -108,7 +110,23 @@ public class Sample_gwt implements EntryPoint {
 			};
 		});
 		
+		
 		return testPanel;
+	}
+	
+	private void rebuildFriendsPanel() {
+		greetingService.getFriends(new AsyncCallback<List<Friend>>() {
+			@Override
+			public void onFailure(Throwable caught) {}
+			
+			public void onSuccess(List<Friend> result) {
+				friendsList.clear();
+				friendsList.add(new Label("-MY FRIENDS-"));
+				for (final Friend friend: result) {
+					friendsList.add(new Label("" + friend.getFriendUserId()));
+				}
+			};
+		});
 	}
 	
 	protected void loadLogin() {
