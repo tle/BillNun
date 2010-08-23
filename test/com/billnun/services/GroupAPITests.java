7c809eca636e2bde3290133a10fc2fbfcab6885b
psadik <bryanrcampbell@gmail.com>
2010-08-23 02:05:52 -0500

    i don't even know

---
+++ test/com/billnun/services/GroupAPITests.java
@@ -0,0 +1,67 @@
+package com.billnun.services;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.testapp.client.pos.UserAccount;
+import com.testapp.client.pos.UserGroup;
+import com.testapp.server.jdo.GroupFactory;
+import com.testapp.server.jdo.UserAccountFactory;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GroupAPITests {
+
+    private final LocalServiceTestHelper helper =
+        new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        helper.tearDown();
+    }
+
+    private void doTest() {
+    	UserGroup g = createTestGroup();
+        System.out.println(g.toString());
+    }
+
+    @Test
+    public void testInsert1() {
+        doTest();
+    }
+
+    @Test
+    public void testInsert2() {
+        doTest();
+    }
+    
+    @Test
+    public void testGet1() {
+    	Collection<UserGroup> g = GroupFactory.getInstance().getAll();
+    	for(UserGroup gg: g) {
+    		System.out.println(gg.toString());
+    	}
+    }
+    
+    public UserGroup createTestGroup() {
+    	UserGroup group = new UserGroup();
+    	group.setName("Group_" + System.currentTimeMillis());
+    	
+    	for(int i=0;i<5;i++) {
+    		String name = "User_" + System.currentTimeMillis();
+    		UserAccount u = new UserAccount(name + "@billnun.test.com", "555-555-5555", name);
+    		UserAccountFactory.getInstance().save(u);
+    		group.addMembers(Collections.singleton(u.getKey()));
+    	}
+    	return GroupFactory.getInstance().save(group);
+    }
+}
